1bca985a130b2fd2baa80f08f1e32c0c16f8946d
stana.adelina.diana@gmail.com <stana.adelina.diana@gmail.com>
2018-11-19 01:55:58 +0200

    changed database insertion

--- LinksDB.py
+++ LinksDB.py
@@ -1,15 +1,21 @@
 import mysql.connector
 from mysql.connector import errorcode
 from collections import OrderedDict
+import csv
 
 
 class LinksDB:
-    def __init__(self):
+    def __init__(self, working_dir):
         self.cnx = mysql.connector.connect(user='root', password='12345', port=3306)
         self.db_cursor = self.cnx.cursor()
+        self.working_dir = working_dir
 
         self.db_name = 'sql_db'
         self.last_idx = 1
+        self.insert_count = 0
+        self.chunk_nr = 0
+        self.current_csv_path = self.working_dir+"/DB"+str(self.last_idx)+".csv"
+        self.writer = csv.writer(open(self.current_csv_path, 'w+'))
 
         self.TABLES = OrderedDict()
 
@@ -17,13 +23,35 @@ class LinksDB:
             self.TABLES['links'+str(i)] = (
                 "CREATE TABLE `links"+str(i)+"` ("
                 "  `name1` VARCHAR(500) NOT NULL,"
-                "  `name2` VARCHAR(500) NOT NULL,"
-                "PRIMARY KEY (name1, name2)"
+                "  `name2` VARCHAR(500) NOT NULL"
                 ") ENGINE=InnoDB")
 
+    def increment_indexes(self):
+        self.last_idx += 1
+        self.current_csv_path = self.working_dir + "/DB" + str(self.last_idx) + ".csv"
+        self.writer = csv.writer(open(self.current_csv_path, 'w+'))
+        self.insert_count = 0
+
+    def load_csv(self):
+        del self.writer
+
+        data = '''LOAD DATA LOCAL INFILE \'''' + str(self.current_csv_path) + '''\' INTO TABLE links''' + str(
+            self.last_idx) + ''' FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' '''
+        try:
+            self.db_cursor.execute(data)
+            self.cnx.commit()
+        except:
+            print("Could not insert csv to database!")
+
+        if self.insert_count > 15000:
+            self.insert_count = 0
+            self.chunk_nr += 1
+            self.current_csv_path = self.working_dir + "/DB" + str(self.last_idx)+"_chunk"+str(self.chunk_nr) + ".csv"
+            self.writer = csv.writer(open(self.current_csv_path, 'w+'))
+
     def count(self):
-        # query = """SELECT COUNT(*) FROM links p1 JOIN links p2 ON p1.name1 = p2.name1 AND p1.name2 < p2.name2"""
-        query = "SELECT COUNT(*) FROM links"+str(self.last_idx)
+        self.load_csv()
+        query = "SELECT COUNT(DISTINCT name1,name2) FROM links"+str(self.last_idx)
         number_of_rows = -1
         try:
             self.db_cursor.execute(query)
@@ -33,22 +61,32 @@ class LinksDB:
         except mysql.connector.Error as err:
             print(err.msg)
 
-        self.last_idx += 1
+        self.increment_indexes()
         return number_of_rows
 
+    def insert(self, name1, name2):
+        self.insert_count += 1
+        self.writer.writerow([name1, name2])
+        if self.insert_count > 15000:
+            self.load_csv()
+    '''
     def insert(self, name1, name2):
         try:
             statement = "INSERT INTO `sql_db`.`links"+str(self.last_idx)+"`(`name1`, `name2`) VALUES ('"+name1+"','"+name2+"')"
             self.db_cursor.execute(statement)
-            self.cnx.commit()
+            self.insert_count += 1
+            if self.insert_count == 5000:
+                self.cnx.commit()
+                self.insert_count = 0
         except mysql.connector.Error as err:
-            return
+            return'''
 
     def create_tables(self):
         print("Creating tables...")
         try:
             for table_name in self.TABLES:
                 self.db_cursor.execute(self.TABLES[table_name])
+                self.db_cursor.execute("CREATE UNIQUE INDEX indexname ON " + table_name+ "(name1,name2)")
             self.cnx.commit()
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
